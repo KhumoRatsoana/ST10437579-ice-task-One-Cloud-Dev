53128c510bbbe8b0a1c3088ea36a62267744fb44
Khumo Ratsoana <166295436+KhumoRatsoana@users.noreply.github.com>
2025-02-20 18:05:37 +0200

    fourth Commit

--- MvcMovie/MvcMovie/Models/ErrorViewModel.cs
+++ MvcMovie/MvcMovie/Models/ErrorViewModel.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
 namespace MvcMovie.Models
 {
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
